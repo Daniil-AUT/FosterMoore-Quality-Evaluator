7d60e605c7786aaccaf9bafe05b56971d3a819ab
Daniil-AUT <ryj5244@autuni.ac.nz>
2024-10-08 20:26:41 +1300

    Update app.py

--- api/backend/app.py
+++ api/backend/app.py
@@ -349,4 +349,5 @@ def verify_credentials():
         return jsonify({"error": "Verification failed."}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT not set
+    app.run(host='0.0.0.0', port=port)
